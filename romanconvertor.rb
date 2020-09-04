b789c0e4784cf8488d28786c5f12729c0680a65f
Nathan Malitz <namalitz@bsu.edu>
2020-09-04 13:21:44 -0400

    Delete convert_from. Add and implement roman_digit_dictionary to/in fromRoman.

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,26 +1,25 @@
-def convert_from(roman_digit)
-    1000 if roman_digit == 'M'
-    500 if roman_digit == 'D'
-    100 if roman_digit == 'C'
-    50 if roman_digit == 'L'
-    10 if roman_digit == 'X'
-    5 if roman_digit == 'V'
-    1 if roman_digit == 'I'
-end
-
 def fromRoman(roman_number)
-    if /^[^IVXLCDM]+$/.match(roman_number).nil? == false | roman_number.empty? | roman_number.nil?
+    if /^[^IVXLCDM]+$/.match(roman_number).nil? == false | roman_number.empty?
         raise TypeError
     end
 
-    roman_character_array = roman_number.split('')
+    roman_digit_dictionary = {
+        'M' => 1000,
+        'D' => 500,
+        'C' => 100,
+        'L' => 50,
+        'X' => 10,
+        'V' => 5,
+        'I' => 1
+    }
+    roman_character_array = roman_number.split(//)
     total = 0
     index = 0
     while index < roman_character_array.length
-        left = convert_from(roman_character_array[index])
+        left = roman_digit_dictionary[roman_character_array[index]]
         right_index = index + 1
         if right_index < roman_character_array.length
-            right = convert_from(roman_character_array[right_index])
+            right = roman_digit_dictionary[roman_character_array[right_index]]
             if left >= right
                 total += left
                 index += 1
