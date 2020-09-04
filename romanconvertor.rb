d55528c762d5ea61a3a0595ccf72eb926f60421a
Nathan Malitz <namalitz@bsu.edu>
2020-09-04 13:59:54 -0400

    Fix implementation of toRoman.

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -37,23 +37,28 @@ end
 
 def toRoman(arabic_number)
     # Replace the following line with the actual code!
-    raise RangeError if arabic_number > 3999 | arabic_number < 1
+    raise RangeError if arabic_number > 3999
+    raise RangeError if arabic_number < 1
 
     roman_string = ''
-    roman_values = [
-        ['M', 1000],
-        ['D', 500],
-        ['C', 100],
-        ['L', 50],
-        ['X', 10],
-        ['V', 5],
-        ['I', 1]
-    ]
+    roman_values = {
+        1000 => "M",
+        900 => "CM",
+        500 => "D",
+        400 => "CD",
+        100 => "C",
+        90 => "XC",
+        50 => "L",
+        40 => "XL",
+        10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
+    }
 
-    roman_values.each do |pair|
-        roman_digit = pair[0]
-        value = pair[1]
-        roman_string += roman_digit * (arabic_number / value)
+    roman_values.each do |value, roman_digit|
+        roman_string << roman_digit * (arabic_number / value)
         arabic_number %= value
     end
     roman_string
